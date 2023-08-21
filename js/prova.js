a0266711a986cb4eb3ef62a6651fc757cc9f12aa
Nicola <62054544+nicolaaltobel@users.noreply.github.com>
2023-08-21 17:41:39 +0200

    Add files via upload

---
+++ js/prova.js
@@ -0,0 +1,48 @@
+
+async function readNFC(){
+    if ("NDEFReader" in window){
+        const ndef = new NDEFReader();
+        try {
+            await ndef.scan();
+            ndef.onreading = event => {
+                const decoder = new TextDecoder();
+                for (const record of event.message.records){
+                    consoleLog("Record type: " + record.recordType);
+                    consoleLog("MIME type: " + record.mediaType);
+                    consoleLog("=== data ===\n"+  decoder.decode(record.data)); 
+                }
+            }
+        } catch (error){
+            consoleLog(error);
+        }
+    } else {
+        consoleLog("Web NFC tag not supported.");
+    }
+}
+
+async function writeNFC(){
+    if ("NDEFReader" in window){
+        const ndef = new NDEFReader();
+        try {
+            await ndef.write("sup");
+            consoleLog("NFC message written");
+            ndef.onreading = event => {
+                const decoder = new TextDecoder();
+                for (const record of event.message.records){
+                    consoleLog("Record type: " + record.recordType);
+                    consoleLog("MIME type: " + record.mediaType);
+                    consoleLog("=== data ===\n"+  decoder.decode(record.data)); 
+                }
+            }
+        } catch (error){
+            consoleLog(error);
+        }
+    } else {
+        consoleLog("Web NFC tag not supported.");
+    }
+}
+
+function consoleLog(data) {
+    var logElement = document.getElementById('log');
+    logElement.innerHTML += data + '\n';
+}
